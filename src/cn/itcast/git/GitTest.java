2ef141596345cab272bfa0f7f8fa50b7fe9ceeb7
taylor <603964937@qq.com>
2019-06-24 20:11:03 +0800

    first commit

---
+++ src/cn/itcast/git/GitTest.java
@@ -0,0 +1,15 @@
+package cn.itcast.git;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+
+        int a=10;
+        for (int i = 0; i < a; i++) {
+            System.out.println(i);
+        }
+    }
+}
